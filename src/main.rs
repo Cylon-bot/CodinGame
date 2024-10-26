f89846d306d5dad44fa65e75806c2663ee617f84
Thibault <thibault.delrieu.pro@gmail.com>
2024-10-27 00:51:56 +0200

    add first maangement of inputs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
+use std::io;
+
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => {
+        $x.trim().parse::<$t>().unwrap()
+    };
+}
+
+struct AshPosition {
+    x: i32,
+    y: i32,
+}
+impl AshPosition {
+    fn new(x: i32, y: i32) -> Self {
+        AshPosition { x, y }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // game loop
+    loop {
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let inputs = input_line.split(" ").collect::<Vec<_>>();
+        let ash = AshPosition::new(parse_input!(inputs[0], i32), parse_input!(inputs[1], i32));
+
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let human_count = parse_input!(input_line, i32);
+        for i in 0..human_count as usize {
+            let mut input_line = String::new();
+            io::stdin().read_line(&mut input_line).unwrap();
+            let inputs = input_line.split(" ").collect::<Vec<_>>();
+            let human_id = parse_input!(inputs[0], i32);
+            let human_x = parse_input!(inputs[1], i32);
+            let human_y = parse_input!(inputs[2], i32);
+        }
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let zombie_count = parse_input!(input_line, i32);
+        for i in 0..zombie_count as usize {
+            let mut input_line = String::new();
+            io::stdin().read_line(&mut input_line).unwrap();
+            let inputs = input_line.split(" ").collect::<Vec<_>>();
+            let zombie_id = parse_input!(inputs[0], i32);
+            let zombie_x = parse_input!(inputs[1], i32);
+            let zombie_y = parse_input!(inputs[2], i32);
+            let zombie_xnext = parse_input!(inputs[3], i32);
+            let zombie_ynext = parse_input!(inputs[4], i32);
+        }
+
+        // Write an action using println!("message...");
+        // To debug: eprintln!("Debug message...");
+
+        println!("0 0"); // Your destination coordinates
+    }
 }
